6266d06698551bf5c646818e5c7121ca7b7eaba0
CodeX93 <xappifai@gmail.com>
2024-11-13 14:56:54 +0500

    added homescreen

---
+++ lib/Home.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class HomeScreem extends StatelessWidget {
+  const HomeScreem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Text('My HomeScreen'),
+      
+      
+    );
+  }
+}
